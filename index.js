1b61d7c44d023deb206ffbdfe79f840518ce97bf
Paul Sauer <paulrsauer@gmail.com>
2025-10-01 00:16:34 -0400

    Ok

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const path = require('path');
 
-process.env.Path += ';';
-process.env.Path += path.resolve(__dirname, 'dist', 'bin').replace('app.asar', 'app.asar.unpacked');
-
 const packageName = 'nops.node';
 const nops = require(`./dist/${packageName}`);
 module.exports = nops;
